6ceba119b99a08fea7f00910bbfedda81614fb6f
Ahmed Ilyas <asilyas98@gmail.com>
2026-09-22 11:39:59 -0400

    Initial commit with Android build workflows

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.simpleai.twentyonequestions',
+  appName: '21 Questions',
+  webDir: 'www'
+};
+
+export default config;
